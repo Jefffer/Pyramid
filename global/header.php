0abbbbf80e789fe23b5ac547db581c3066498e74
jalhem <jalhem07@gmail.com>
2019-07-26 20:11:05 -0500

    changes

--- global/header.php
+++ global/header.php
@@ -40,8 +40,8 @@
 
              <div class="contenido-header">
                 <nav class="redes-sociales">
-                   <a href=""><i class="fab fa-facebook" aria-hidden="true"></i></a>
-                   <a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+                   <a href="https://www.facebook.com/viatainmobiliaria"><i class="fab fa-facebook" aria-hidden="true"></i></a>
+                   <a href="https://twitter.com/Viatainmobi"><i class="fab fa-twitter" aria-hidden="true"></i></a>
                    <!--<a href="#"><i class="fab fa-youtube" aria-hidden="true"></i></a>-->
                    <a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
                 </nav>
